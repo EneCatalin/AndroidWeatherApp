4a9efc9300d792d246eb3b65a62dd0d803ef99e6
Chris <enecatalin9393@gmail.com>
2018-09-11 16:48:48 +0300

    managed to write(ie copy) a unix time converter. Now I just need to attach it to my rv time and I am set. After that I am supposed to make a layout for my small screen phone but there's no way I do that today, tomorrow it is (he said the 50th time in a row). No, today I will probably try (and fail naturally) to make the city completer

---
+++ app/src/main/java/com/example/android/fragmentpasswithviewmodel/activities/UnixTimeConverterScriptAct.java
@@ -0,0 +1,43 @@
+package com.example.android.fragmentpasswithviewmodel.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.android.fragmentpasswithviewmodel.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UnixTimeConverterScriptAct extends AppCompatActivity {
+
+    private static final String TAG = "UnixTimeAct";
+    EditText unixTimeUnit;
+    TextView convertedDayTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unix_time_converter_script);
+
+        unixTimeUnit=(EditText) findViewById(R.id.unixEditText);
+        convertedDayTV=(TextView)findViewById(R.id.convertedDayTV);
+    }
+
+    public void unixConvert(View view) {
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+//        Date dateFormat = new java.util.Date(unixTimeUnit.getText().toString());
+        Date dateFormat = new java.util.Date(Long.parseLong(unixTimeUnit.getText().toString())*1000);
+//        Log.d(TAG, String.valueOf(dateFormat));
+        String weekday = sdf.format(dateFormat);
+        Log.d(TAG, String.valueOf(weekday));
+
+
+        unixTimeUnit.getText();
+//        Log.d(TAG, String.valueOf(unixTimeUnit.getText()).trim());
+//        convertedDayTV.setText((c.get(Calendar.DAY_OF_WEEK)));
+    }
+}
